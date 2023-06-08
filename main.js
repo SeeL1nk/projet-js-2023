c604e5df475089b69e167a868406d6baa2b7712e
Loïc GAO <126685637+SeeL1nk@users.noreply.github.com>
2023-06-08 16:44:27 +0200

    read .data file

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const R = require('ramda');
+const fs = require('fs');
+
+fs.readFile('connect-4.data', 'utf8', (err, data) => {
+
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(data);
+});
+
